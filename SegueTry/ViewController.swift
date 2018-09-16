57e1b492588b266868a269126a14bbda712e336a
Prasath Srithar <prasathsrithar@hotmail.co.uk>
2018-09-16 18:01:15 +0100

    Segue example first attempt

--- SegueTry/ViewController.swift
+++ SegueTry/ViewController.swift
@@ -9,4 +9,11 @@ class ViewController: UIViewController {
     @IBAction func click_me_button(_ sender: Any) {
     }
 
+
+    // MARK:
+    // 1. Use PrepareSegue function to pass data to the other view controller
+    // 2. Add an imageview to second view controller
+    // 3. Set contraints for button to be in the middle and text to be at the top
+    // 4. Make text resize automatically so it fits in the screen properly 
+
 }
